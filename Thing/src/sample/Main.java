b83cc854bfcd710b0d70f82d433248983eb43742
kyrre <kyrre.l.moe@gmail.com>
2016-10-20 15:33:05 +0200

    border

--- Thing/src/sample/Main.java
+++ Thing/src/sample/Main.java
@@ -3,33 +3,24 @@ package sample;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.concurrent.Task;
 import javafx.concurrent.WorkerStateEvent;
 import javafx.event.ActionEvent;
-import javafx.event.Event;
 import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 import model.World;
 import model.WorldState;
-import sun.reflect.generics.tree.ReturnType;
 
 import java.util.ArrayList;
-import java.util.Random;
-
-import static javafx.scene.input.KeyCode.T;
 
 public class Main extends Application {
 
-    private static final int SIZE = 20;
+    private static final int SIZE = 30;
     private static final int  NUMBER_OF_TICKS = 120;
     private static final javafx.util.Duration FREQUENCY = Duration.millis(500);
 
@@ -37,7 +28,7 @@ public class Main extends Application {
     private static int height = SIZE;
 
     private static int playBackIndex = 0;
-    private static TextField[][] inputCells = new TextField[width][height];
+    private static Label[][] outputCells = new Label[width][height];
     private static ArrayList<WorldState> worldStates = new ArrayList<>();
 
     @Override
@@ -48,22 +39,30 @@ public class Main extends Application {
         for(int y = 0; y < height; y++){
             for(int x = 0; x < width; x++){
 
+                Label outputCell = new Label("-");
+                outputCell.setPrefHeight(20);
+                outputCell.setPrefWidth(20);
+                outputCell.setAlignment(Pos.CENTER);
+                outputCell.setStyle("-fx-border-color: lightgray;");
                 // Create a new TextField in each Iteration
-                TextField tf = new TextField();
-                tf.setPrefHeight(50);
-                tf.setPrefWidth(50);
-                tf.setAlignment(Pos.CENTER);
-                tf.setEditable(false);
-                tf.setText("---");
-                inputCells[x][y] = tf;
+//                TextField tf = new TextField();
+//                tf.setPrefHeight(50);
+//                tf.setPrefWidth(50);
+//                tf.setAlignment(Pos.CENTER);
+//                tf.setEditable(false);
+//                tf.setText("---");
+
+                outputCells[x][y] = outputCell;
+
+
                 // Iterate the Index using the loops
-                root.setRowIndex(tf,height-1-y);
-                root.setColumnIndex(tf,x);
-                root.getChildren().add(tf);
+                root.setRowIndex(outputCell,height-1-y);
+                root.setColumnIndex(outputCell,x);
+                root.getChildren().add(outputCell);
             }
         }
 
-        Scene scene = new Scene(root, 900, 900);
+        Scene scene = new Scene(root, 900, 500);
         primaryStage.setTitle("Random Binary Matrix (JavaFX)");
         primaryStage.setScene(scene);
         primaryStage.show();
@@ -84,7 +83,7 @@ public class Main extends Application {
                                     WorldState worldState = worldStates.get(playBackIndex);
                                     for (int x = 0; x < worldState.cellStates.length; x++) {
                                         for (int y = 0; y < worldState.cellStates[0].length; y++) {
-                                            inputCells[x][y].setText(worldState.cellStates[x][y].toString());
+                                            outputCells[x][y].setText(worldState.cellStates[x][y].toString());
                                         }
                                     }
                                     playBackIndex++;
@@ -107,11 +106,11 @@ public class Main extends Application {
             public void handle(WorkerStateEvent event) {
                 ArrayList<WorldState> newWorldStates = (ArrayList<WorldState>) event.getSource().getValue();
                 worldStates.addAll(newWorldStates);
-                int count = 0;
-                for (WorldState w : worldStates) {
-                    System.out.println("WorldState "+count+++":");
-                    System.out.println(w);
-                }
+//                int count = 0;
+//                for (WorldState w : worldStates) {
+//                    System.out.println("WorldState "+count+++":");
+//                    System.out.println(w);
+//                }
             }
         });
         simulator.setOnFailed(new EventHandler<WorkerStateEvent>() {
