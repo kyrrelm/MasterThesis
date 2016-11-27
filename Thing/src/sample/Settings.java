650eace44d586b77cf9f272eb0a54dca545a12b6
kyrre <kyrre.l.moe@gmail.com>
2016-11-27 22:27:02 +0100

    more logging

--- Thing/src/sample/Settings.java
+++ Thing/src/sample/Settings.java
@@ -18,9 +18,10 @@ public class Settings {
     public static final int NUMBER_OF_SCOUTS = 8;
     public static final int NUMBER_OF_HARVESTERS = 2;
 
+    public static final boolean DIFFUSE_BROWN = false;
+
     public static final int RECRUIT_SIZE = 1;
     public static final boolean CONSTANT_RECRUITMENT = true;
-    public static final boolean DIFFUSE_BROWN = false;
 
     public static final boolean USING_APF_VALUE = true;
 
@@ -56,12 +57,15 @@ public class Settings {
     public static String getLog() {
         String output =  "Map: "+ MAP.name + "\nTotal food count: " + MAP.foodCount
                 +"\nFood range: " + FOOD_COEFFICIENT + " - " + FOOD_COEFFICIENT*9
+                + "\nUsing pheromones: "+ !USING_APF_VALUE
+                + "\nSCOUTS:"
                 + "\nNumber of scouts: " + NUMBER_OF_SCOUTS
-                +"\nScout capacity: "+ SCOUT_CAPACITY
+                + "\nScout capacity: "+ SCOUT_CAPACITY
+                + "\nDiffuse brown: " + DIFFUSE_BROWN
+                + "\nHARVESTERS:"
                 + "\nNumber of harvesters: "+ NUMBER_OF_HARVESTERS
                 + "\nHarvester capacity: " + HARVESTER_CAPACITY
-                + "\nHarvester recruit size: " + RECRUIT_SIZE
-                + "\nUsing pheromones: "+ !USING_APF_VALUE;
+                + "\nHarvester recruit size: " + RECRUIT_SIZE;
         return output;
     }
 }
